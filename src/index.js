9a09b3e889c3db27c949f37efdefa7af7eb41e25
dayounglee <verdantjuly@kakao.com>
2024-10-08 15:08:33 +0900

    [Feat] tick

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function tick() {
+  const content = (
+    <div>
+      <h1>Hello,React</h1>
+      <h2>current Time: {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(content);
+}
+
+setInterval(tick, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
